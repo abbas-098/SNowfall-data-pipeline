e26d13592caa7c4677a16d228500ce7a82023870
Mohammed Abbas <abbas-97@outlook.com>
2024-03-15 01:09:29 +0000

    updates script

--- terraform/core_delta_lake/appflow/main.tf
+++ terraform/core_delta_lake/appflow/main.tf
@@ -1,19 +1,26 @@
 resource "null_resource" "create_appflow" {
     count = 1
     provisioner "local-exec" {
-        command = "python3 create_appflow.py"
+        command = <<-EOT
+            cd appflow &&
+            python3 create_appflow.py
+        EOT
+        
         environment = {
           LandingBucket = var.landing_bucket_name
           ConnectorProfileName = var.connector_profile_name
         }
-      
     }
 }
+
 
 resource "null_resource" "delete_appflow" {
     count = 1
     provisioner "local-exec" {
-        command = "python3 delete_appflow.py"
+        command = <<-EOT
+            cd appflow &&
+            python3 delete_appflow.py
+        EOT
         when = destroy
     }
 }
