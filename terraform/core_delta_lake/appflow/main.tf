ebd87c68a93f703917cf6c5ab5a0a08ec0d24c84
Mohammed Abbas <abbas-97@outlook.com>
2024-03-15 01:15:53 +0000

    fixes

--- terraform/core_delta_lake/appflow/main.tf
+++ terraform/core_delta_lake/appflow/main.tf
@@ -1,10 +1,7 @@
 resource "null_resource" "create_appflow" {
     count = 1
     provisioner "local-exec" {
-        command = <<-EOT
-            cd appflow &&
-            python3 create_appflow.py
-        EOT
+        command = "python3 ${path.module}/create_appflow.py"
         
         environment = {
           LandingBucket = var.landing_bucket_name
@@ -17,10 +14,7 @@ resource "null_resource" "create_appflow" {
 resource "null_resource" "delete_appflow" {
     count = 1
     provisioner "local-exec" {
-        command = <<-EOT
-            cd appflow &&
-            python3 delete_appflow.py
-        EOT
+        command = "python3 ${path.module}/delete_appflow.py"
         when = destroy
     }
 }
