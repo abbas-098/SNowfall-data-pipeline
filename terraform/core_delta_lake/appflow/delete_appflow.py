2dd781bec3262c3b2b3bd10fcc1126805c580f81
Mohammed Abbas <abbas-97@outlook.com>
2024-03-19 16:09:25 +0000

    changes

--- terraform/core_delta_lake/appflow/delete_appflow.py
+++ terraform/core_delta_lake/appflow/delete_appflow.py
@@ -3,9 +3,6 @@ import json
 import os
 import logging
 
-# Configure logging
-logger = logging.getLogger()
-logger.setLevel("INFO")
 
 client = boto3.client('appflow', region_name='eu-central-1')
 config_file_path = f"{os.getcwd()}/appflow/flow_config.json"
@@ -13,11 +10,11 @@ config_file_path = f"{os.getcwd()}/appflow/flow_config.json"
 def delete_flow(flow_name):
     try:
         client.delete_flow(flowName=flow_name,forceDelete=True)
-        logger.info(f"Flow '{flow_name}' deleted successfully.")
+        print(f"Flow '{flow_name}' deleted successfully.")
     except client.exceptions.ResourceNotFoundException:
-        logger.info(f"Flow '{flow_name}' not found.")
+        print(f"Flow '{flow_name}' not found.")
     except Exception as e:
-        logger.info(f"Failed to delete flow '{flow_name}': {e}")
+        print(f"Failed to delete flow '{flow_name}': {e}")
 
 # Load JSON files
 with open(config_file_path) as f:
@@ -29,4 +26,4 @@ for flow_config in flows_config:
     if flow_name:
         delete_flow(flow_name)
     else:
-        logger.info("Flow name not found in configuration:", flow_config)
+        print("Flow name not found in configuration:", flow_config)
