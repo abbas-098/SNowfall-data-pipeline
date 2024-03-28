276369b1274a1ec0534e0eb56fa16bb676c304de
Mohammed Abbas <abbas-97@outlook.com>
2024-03-28 12:13:46 +0000

    added email

--- terraform/core_delta_lake/sns/main.tf
+++ terraform/core_delta_lake/sns/main.tf
@@ -7,5 +7,5 @@ resource "aws_sns_topic" "snowfall_topic" {
 resource "aws_sns_topic_subscription" "email-target" {
   topic_arn = aws_sns_topic.snowfall_topic.arn
   protocol  = "email"
-  endpoint  = "abbas-97@outlook.com"
+  endpoint  = "mohammed.abbas@capgemini.com"
 }
